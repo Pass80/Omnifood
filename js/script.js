acfb9481f176a987a26ecc5e7fc60793b66ad239
Feras <ferassaleh1980@gmail.com>
2022-09-07 20:15:48 +0200

    add some functionalities and favicon to the page

--- js/script.js
+++ js/script.js
@@ -47,14 +47,38 @@ const obs = new IntersectionObserver(
     (entries) => {
         const ent = entries[0];
         console.log(ent);
-        if (ent.isIntersecting === false) {
-            document.querySelector('.header').classList.add('sticky');
+        if (!ent.isIntersecting) {
+            document.body.classList.add('sticky');
+        }
+        if (ent.isIntersecting) {
+            document.body.classList.remove('sticky');
         }
     },
     {
         // in the viewport
         root: null,
         threshold: 0,
+        rootMargin: '-80px',
     }
 );
 obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+    var flex = document.createElement('div');
+    flex.style.display = 'flex';
+    flex.style.flexDirection = 'column';
+    flex.style.rowGap = '1px';
+
+    flex.appendChild(document.createElement('div'));
+    flex.appendChild(document.createElement('div'));
+
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
+
+    if (!isSupported) document.body.classList.add('no-flexbox-gap');
+}
+checkFlexGap();
